590f12d7d705b4a4d9382dcb0cff1f84a2aa848e
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-10-04 10:20:28 +0200

    last update

---
+++ src/test/java/com/scc/show/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.scc.show;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+	/*
+	 * Référence documentation: http://www.baeldung.com/spring-boot-testing
+	 */
+
+	@Test
+	public void contextLoads() {
+	}
+}
