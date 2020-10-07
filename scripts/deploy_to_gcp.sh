a343e392db4d7401612e7ef6e06dd992b997847c
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-10-07 18:33:17 +0200

    :construction_worker: still progress

--- scripts/deploy_to_gcp.sh
+++ scripts/deploy_to_gcp.sh
@@ -6,17 +6,17 @@ export LOCAL_REPO=../gcp-migration
 export myConfigFile=./terraform/services-vars.tf
 
 
-echo "INFO- Checking secret env. variables..."
-if [ "q${GITHUB_TOKEN}" == "q" ]
-then
-  echo "ERROR- Secret variable GITHUB_TOKEN is empty!"
-  exit 204
-fi
+#echo "INFO- Checking secret env. variables..."
+#if [ "q${GITHUB_TOKEN}" == "q" ]
+#then
+#  echo "ERROR- Secret variable GITHUB_TOKEN is empty!"
+#  exit 204
+#fi
 
 myTagRoot=`echo ${BUILD_NAME} | sed  's/^\([a-z-]*\)[0-9-]*$/\1/g'`
 
-echo "INFO- Clone git project: ................... ${GITHUB_REPO}"
-git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} ${LOCAL_REPO} --branch ${GCP_ENV} --single-branch 2>&1
+#echo "INFO- Clone git project: ................... ${GITHUB_REPO}"
+#git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} ${LOCAL_REPO} --branch ${GCP_ENV} --single-branch 2>&1
 
 echo "INFO- Update docker image tag in file: ..... ${myConfigFile}"
 echo "INFO- New docker image tag: ................ ${BUILD_NAME}"
@@ -28,8 +28,8 @@ sed -i "s/${myTagRoot}[0-9-]*/${SHORT_SHA}/" ${myConfigFile} 2>&1
 echo "INFO- Push file: ......... ${myConfigFile}"
 echo "INFO- ... to branch: ..... ${GCP_ENV}"
 
-git config user.email "anthony.denecheau@centrale-canine.fr"
-git config user.name "${GITHUB_USER}"
-git add .
-git commit -m":rocket: :wrench: :arrow_up: changed application version" .
-git push
+#git config user.email "anthony.denecheau@centrale-canine.fr"
+#git config user.name "${GITHUB_USER}"
+#git add .
+#git commit -m":rocket: :wrench: :arrow_up: changed application version" .
+#git push
