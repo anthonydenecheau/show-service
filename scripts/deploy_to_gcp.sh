c69a8aed21bca2a1485d4300dbe8ecf31074288f
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-10-08 09:18:16 +0200

    :rocket:

--- scripts/deploy_to_gcp.sh
+++ scripts/deploy_to_gcp.sh
@@ -5,8 +5,6 @@ export GITHUB_REPO=github.com/centrale-canine/gcp-migration.git
 export LOCAL_REPO=gcp-migration
 export myConfigFile=./terraform/services-vars.tf
 
-myTagRoot=`echo ${BUILD_NAME} | sed  's/^\([a-z-]*\)[0-9-]*$/\1/g'`
-
 echo "INFO- Clone git project: ................... ${GITHUB_REPO}"
 
 cd /workspace
@@ -14,13 +12,17 @@ git clone -b ${GCP_ENV} git@github.com:centrale-canine/gcp-migration
 
 echo "INFO- Update docker image tag in file: ..... ${myConfigFile}"
 echo "INFO- New docker image tag: ................ ${BUILD_NAME}"
-echo "INFO- pwd: ................................. $(pwd)"
-echo "INFO- fs: ................................. $(ls -la)"
+#echo "INFO- pwd: ................................. $(pwd)"
+#echo "INFO- fs: .................................. $(ls -la)"
 
 cd ${LOCAL_REPO}
-$(cat ${myConfigFile})
+
+myTagRoot=`echo ${BUILD_NAME} | sed  's/^\([a-z-]*\)[0-9-]*$/\1/g'`
+echo "INFO- myTagRoot: .................................. ${myTagRoot}"
+
+echo "INFO- Original myConfigFile: .................................. $(cat ./terraform/services-vars.tf)"
 sed -i "s/${myTagRoot}[0-9-]*/${SHORT_SHA}/" ${myConfigFile} 2>&1
-$(cat ${myConfigFile})
+echo "INFO- Final myConfigFile: .................................. $(cat ./terraform/services-vars.tf)"
 
 echo "INFO- Push file: ......... ${myConfigFile}"
 echo "INFO- ... to branch: ..... ${GCP_ENV}"
