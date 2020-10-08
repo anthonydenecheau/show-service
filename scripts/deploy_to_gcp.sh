c8df4b92a1713d566373af41417dc510c4cd7e23
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-10-08 10:48:58 +0200

    :rocket: ready to deploy

--- scripts/deploy_to_gcp.sh
+++ scripts/deploy_to_gcp.sh
@@ -20,8 +20,8 @@ sed -i "s/${myTagRoot}[a-z0-9:-]*/${TAG_NAME}/" ${myConfigFile} 2>&1
 echo "INFO- Push file: ......... ${myConfigFile}"
 echo "INFO- ... to branch: ..... ${GCP_ENV}"
 
-#git config user.email "anthony.denecheau@centrale-canine.fr"
-#git config user.name "${GITHUB_USER}"
-#git add .
-#git commit -m":rocket: :wrench: :arrow_up: changed application version from GCP BUILD" .
-#git push
+git config user.email "anthony.denecheau@centrale-canine.fr"
+git config user.name "${GITHUB_USER}"
+git add .
+git commit -m":rocket: :wrench: :arrow_up: changed application version from GCP BUILD" .
+git push
