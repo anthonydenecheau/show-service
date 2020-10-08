0e75bcd33ddf81473c6f970dc9f74a70de62969b
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-10-08 09:03:05 +0200

    :rocket:

--- scripts/deploy_to_gcp.sh
+++ scripts/deploy_to_gcp.sh
@@ -2,18 +2,18 @@
 
 export GITHUB_USER=gcloud-repo
 export GITHUB_REPO=github.com/centrale-canine/gcp-migration.git
-export LOCAL_REPO=../gcp-migration
+export LOCAL_REPO=gcp-migration
 export myConfigFile=./terraform/services-vars.tf
 
 myTagRoot=`echo ${BUILD_NAME} | sed  's/^\([a-z-]*\)[0-9-]*$/\1/g'`
 
 echo "INFO- Clone git project: ................... ${GITHUB_REPO}"
-#git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} ${LOCAL_REPO} --branch ${GCP_ENV} --single-branch 2>&1
-git clone git@github.com:centrale-canine/gcp-migration
+git clone -b ${GCP_ENV} git@github.com:centrale-canine/gcp-migration
 
 echo "INFO- Update docker image tag in file: ..... ${myConfigFile}"
 echo "INFO- New docker image tag: ................ ${BUILD_NAME}"
 echo "INFO- pwd: ................................. $(pwd)"
+echo "INFO- fs: ................................. $(ls -la)"
 
 cd ${LOCAL_REPO}
 
